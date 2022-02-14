dbea5ef94ab560f3b42d2d16c29441b1d68982a3
Bary <baryman12@gmail.com>
2022-02-14 03:11:48 +0300

    123

--- welcome/models.py
+++ welcome/models.py
@@ -11,4 +11,7 @@ class Task(models.Model):
     task = models.TextField('Описание')
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
 
