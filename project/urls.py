89be2c98e693df8feb0a1f869c3bb81b386ea788
Bary <baryman12@gmail.com>
2022-02-14 04:47:32 +0300

    123

--- project/urls.py
+++ project/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
+    url(r'^$', include('welcome.urls')),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
 ]
